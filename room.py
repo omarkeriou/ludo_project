be6080abe211cba812b632ea660d859f19e13682
omarkeriou <omar.keriou@outlook.de>
2021-05-11 20:38:03 +0200

    room has now a new attribute named joinable which reflects if there is a game running in the room or not

--- room.py
+++ room.py
@@ -1,21 +1,39 @@
+from LudoScheduler import Ludo_Scheduler
 class room:
     __id=0
-    def __init__(self):
+    def __init__(self,max=4):
         self.number=room.__id
         room.__id+=1
         self.numberOfPlayer=0
         self.free=True
-        self.max=4
+        self.joinable=False
+        self.max=max
+        self.GameScheduler=Ludo_Scheduler()
 
     def add_Player(self):
-        if self.numberOfPlayer< self.max:
+        if self.numberOfPlayer< self.max and self.joinable:
             self.numberOfPlayer+=1
         else:
             self.free=False
+            self.joinable=False
+            self.GameScheduler.addRoom(self)
+
     def removePlayer(self):
         if self.numberOfPlayer>0:
             self.numberOfPlayer-=1
             self.free=True
+        else:
+            self.GameScheduler.removeRoom(self)
+
+
+    def startPlaying(self):
+        if self.free==False:
+            self.joinable = False
+
+
+    def stopPlaying(self):
+        self.joinable = True
+
 
 
 
